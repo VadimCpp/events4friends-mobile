df2e859c92c4588b570d737720b18e1a160684a1
Vadim Cpp <vadim.cpp@gmail.com>
2020-04-15 21:42:54 +0200

    #13 add image with gradient

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Button from '../../components/Button';
 import MainBackground from '../../components/MainBackground';
 
@@ -14,6 +14,16 @@ export default function HomeScreen(props: HomeScreenParams) {
     <View style={styles.container}>
       <MainBackground />
       <Text>Welcome to events4friends!</Text>
+      <View>
+        <Image
+          style={styles.imgBike}
+          source={require('../../assets/img/bike.png')}
+        />
+        <Image
+          style={styles.imgBikeGradient}
+          source={require('../../assets/img/bike_gradient.png')}
+        />
+      </View>
       <Button
         color="#EC7B28"
         title="перейти"
@@ -38,5 +48,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imgBike: {
+    width: 295,
+    height: 191,
+    borderRadius: 9,
+  },
+  imgBikeGradient: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 295,
+    height: 191,
+    borderRadius: 9,
   },
 });
