4d0c220007ebaa3c7836e16879d4f0cdb6809499
VadimPhilippov <vadfil@gmail.com>
2020-04-15 23:05:54 +0200

    # 14 Create button and navigation props.

--- screens/Events/index.tsx
+++ screens/Events/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import Button from '../../components/Button';
 
-export default function EventsScreen() {
+interface EventsScreenParams {
+  navigation: any;
+}
+
+export default function EventsScreen(props: EventsScreenParams) {
+  const { navigation } = props;
+
   return (
     <View style={styles.container}>
       <Text>Events Screen</Text>
+      <Button
+        color="#EC7B28"
+        title="подробнее"
+        onPress={() => {
+          navigation.navigate('EventSingleScreen');
+        }}
+      />
     </View>
   );
 }
