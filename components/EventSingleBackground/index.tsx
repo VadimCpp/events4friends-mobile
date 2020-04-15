ea21b3fe17b653e0286ba99ccd5a313654f0f63f
VadimPhilippov <vadfil@gmail.com>
2020-04-16 00:14:58 +0200

    #14 Create a component EventSingleBackground.

---
+++ components/EventSingleBackground/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('screen');
+
+const FIGMA_WIDTH = 375;
+
+export default function EventSingleBackground() {
+  return (
+    <View style={styles.background}>
+      <Image
+        style={styles.wave1Style}
+        source={require('../../assets/img/wave1SingleEvent_x4.png')}
+      />
+      <Image
+        style={styles.wave2Style}
+        source={require('../../assets/img/wave2SingleEvent_x4.png')}
+      />
+      <Image
+        style={styles.wave3Style}
+        source={require('../../assets/img/wave3SingleEvent_x4.png')}
+      />
+      <Image
+        style={styles.vectorStyle}
+        source={require('../../assets/img/VectorSingleEvent_x4.png')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width,
+  },
+  wave1Style: {
+    position: 'absolute',
+    top: width * (517 / FIGMA_WIDTH),
+    left: 0,
+    width,
+    height: width * (150 / FIGMA_WIDTH),
+  },
+  wave2Style: {
+    position: 'absolute',
+    top: width * (363 / FIGMA_WIDTH),
+    left: 0,
+    width,
+    height: width * (150 / FIGMA_WIDTH),
+  },
+  wave3Style: {
+    position: 'absolute',
+    top: width * (285 / FIGMA_WIDTH),
+    left: 0,
+    width,
+    height: width * (60 / FIGMA_WIDTH),
+  },
+  vectorStyle: {
+    position: 'absolute',
+    top: width * (18 / FIGMA_WIDTH),
+    left: width * (375 / FIGMA_WIDTH),
+    width,
+    height: width * (226 / FIGMA_WIDTH),
+  },
+});
