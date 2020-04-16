89458547b6bc84fb228521447dbabcd16185734e
VadimPhilippov <vadfil@gmail.com>
2020-04-16 23:33:26 +0200

    #17 Adjusted top positions of the elements.

--- components/SingleBackground/index.tsx
+++ components/SingleBackground/index.tsx
@@ -17,10 +17,6 @@ export default function EventSingleBackground() {
         source={require('../../assets/img/wave2SingleEvent_x4.png')}
       />
       <Image
-        style={styles.wave3Style}
-        source={require('../../assets/img/wave3SingleEvent_x4.png')}
-      />
-      <Image
         style={styles.vectorStyle}
         source={require('../../assets/img/VectorSingleEvent_x4.png')}
       />
@@ -44,22 +40,15 @@ const styles = StyleSheet.create({
   },
   wave2Style: {
     position: 'absolute',
-    top: width * (363 / FIGMA_WIDTH),
+    top: width * (195 / FIGMA_WIDTH),
     left: 0,
     width: '100%',
     height: width * (150 / FIGMA_WIDTH),
   },
-  wave3Style: {
-    position: 'absolute',
-    top: width * (285 / FIGMA_WIDTH),
-    left: 0,
-    width: '100%',
-    height: width * (60 / FIGMA_WIDTH),
-  },
   vectorStyle: {
     position: 'absolute',
-    top: width * (18 / FIGMA_WIDTH),
-    left: width * (0 / FIGMA_WIDTH),
+    top: 0,
+    left: 0,
     width: '100%',
     height: width * (226 / FIGMA_WIDTH),
   },
