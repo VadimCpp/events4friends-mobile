5f7280c3827439abd53a565d0ae173c99366beab
Vadim Cpp <vadim.cpp@gmail.com>
2020-04-20 21:50:23 +0200

    #24 add element box and summary

--- screens/Events/index.tsx
+++ screens/Events/index.tsx
@@ -13,17 +13,22 @@ export default function EventsScreen(props: EventsScreenParams) {
   return (
     <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
-        <Text>Events Screen</Text>
         <DataContext.Consumer>
           {({ events }) => {
             return events.map((event: any) => {
               return (
-                <View key={event.id}>
-                  <Text>...</Text>
+                <View key={event.id} style={styles.listItemContainer}>
+                  <View style={styles.summaryWrap}>
+                    <Text style={styles.summary} numberOfLines={1}>
+                      {event.summary}
+                    </Text>
+                  </View>
+                  <View style={styles.hr} />
+                  {/* <Text>...</Text>
                   <Text>Тут будет компонент элемента списка EventListItem</Text>
                   <Text>...</Text>
                   <Text>{event.summary}</Text>
-                  <Text>{event.description}</Text>
+                  <Text>{event.description}</Text> */}
                 </View>
               );
             });
@@ -49,5 +54,38 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  //
+  // TODO: move to component
+  //
+  listItemContainer: {
+    width: 300,
+    height: 200,
+    borderRadius: 9,
+    marginTop: 16,
+    backgroundColor: 'white',
+
+    shadowColor: '#444',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  summaryWrap: {
+    height: 40,
+    paddingTop: 11,
+    paddingHorizontal: 13,
+  },
+  summary: {
+    fontSize: 18,
+    color: '#404040',
+    fontWeight: 'bold',
+  },
+  hr: {
+    width: 282,
+    height: 2,
+    backgroundColor: '#EC7B28',
+    marginHorizontal: 9,
   },
 });
