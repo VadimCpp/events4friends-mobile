0000721b0ef49aa55e3e2168dcd498c7308844b6
Vadim Cpp <vadim.cpp@gmail.com>
2020-04-23 22:16:11 +0200

    #42 add scroll view and inner container

--- screens/ServiceSingle/index.tsx
+++ screens/ServiceSingle/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import SingBackground from '../../components/SingleBackground';
+import { Text, View, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import SingleBackground from '../../components/SingleBackground';
+
+const { width } = Dimensions.get('screen');
+
+const FIGMA_WIDTH = 375;
 
 interface ServiceSingleScreenParams {
   route: any;
@@ -13,8 +17,17 @@ export default function ServiceSingleScreen(props: ServiceSingleScreenParams) {
 
   return (
     <View style={styles.container}>
-      <SingBackground />
-      <Text>{service.name}</Text>
+      <SingleBackground />
+      <ScrollView
+        style={styles.scrollViewContainer}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View style={styles.innerContainer}>
+          <Text>{service.service}</Text>
+          <Text>{service.name}</Text>
+          <Text>{service.description}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -25,5 +38,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scrollViewContainer: {
+    width: '100%',
+  },
+  contentContainer: {
+    alignItems: 'center',
+  },
+  innerContainer: {
+    width: width * (315 / FIGMA_WIDTH),
+    backgroundColor: 'white',
+    marginTop: 30,
+    marginBottom: 50,
+    marginHorizontal: width * (30 / FIGMA_WIDTH),
+    borderRadius: width * (10 / FIGMA_WIDTH),
+
+    shadowColor: '#444',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 5,
   },
 });
