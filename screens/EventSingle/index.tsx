468bf2292a03dfb09d907d269dd3fd7e6b6a6d13
Vadim Cpp <vadim.cpp@gmail.com>
2020-09-28 23:00:12 +0300

    #94 display description as html

--- screens/EventSingle/index.tsx
+++ screens/EventSingle/index.tsx
@@ -8,7 +8,9 @@ import {
   ScrollView,
   AsyncStorage,
   Alert,
+  Dimensions,
 } from 'react-native';
+import HTML from 'react-native-render-html';
 import moment from 'moment';
 import { Linking } from 'expo';
 import { removeTags, calcSize, timeZoneToCityName } from '../../utils/Misc';
@@ -27,10 +29,10 @@ export default function EventSingleScreen(props: EventSingleScreenParams) {
   const [reminder, setReminder] = useState(false);
   const [disabled, setDisabled] = useState(false);
 
-  const startDate = moment(`${event.start}`).format(
-    'D MMMM, dddd',
-  );
-  const startTime = `${moment(event.start).format('HH:mm')} ${timeZoneToCityName(event.timezone)}`;
+  const startDate = moment(`${event.start}`).format('D MMMM, dddd');
+  const startTime = `${moment(event.start).format(
+    'HH:mm',
+  )} ${timeZoneToCityName(event.timezone)}`;
 
   useEffect(() => {
     (async function asyncWrapper() {
@@ -100,14 +102,21 @@ export default function EventSingleScreen(props: EventSingleScreenParams) {
           </View>
           <View style={styles.hr} />
           <View style={styles.descriptionContainer}>
-            <Text style={styles.summary}>{event.summary}</Text>
-            <Text style={styles.description}>
-              {removeTags(event.description)}
-            </Text>
+            <View>
+              <Text style={styles.summary}>{event.summary}</Text>
+            </View>
+            <ScrollView style={styles.description}>
+              <HTML
+                html={event.description}
+                imagesMaxWidth={Dimensions.get('window').width}
+              />
+            </ScrollView>
             {event.name && (
-              <Text style={styles.description}>
-                {`Организатор мероприятия:\n${event.name}`}
-              </Text>
+              <View>
+                <Text style={styles.description}>
+                  {`Организатор мероприятия:\n${event.name}`}
+                </Text>
+              </View>
             )}
           </View>
           {event.isOnline && (
@@ -270,6 +279,7 @@ const styles = StyleSheet.create({
     color: '#404040',
   },
   description: {
+    flex: 1,
     marginTop: 20,
     color: '#404040',
   },
