d382cdeb80ef28df65f10897d8ee287886d3d15d
Vadim Cpp <vadim.cpp@gmail.com>
2021-03-07 21:31:02 +0300

    #109 use auth context in events view

--- screens/Events/index.tsx
+++ screens/Events/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import moment from 'moment';
 import DataContext from '../../context/DataContext';
 import EventsListItem from '../../components/EventsListItem';
 import Button from '../../components/Button';
 import { calcSize } from '../../utils/Misc';
+import AuthContext from '../../context/AuthContext';
 
 enum EventsFilter {
   Upcoming = 'UPCOMING_EVENTS',
@@ -12,103 +13,116 @@ enum EventsFilter {
   // TODO: add more types here
 }
 
+const NOTICES = {
+  CONNECT: 'Подключаемся к базе данных...',
+  LOADING: 'Загружаем события...',
+};
+
 interface EventsScreenParams {
   navigation: any;
 }
 
 export default function EventsScreen(props: EventsScreenParams) {
   const { navigation } = props;
+  const authContext = useContext(AuthContext);
+  const { connectingToFirebase } = authContext;
   const [filterType, setFilterType] = useState(EventsFilter.Upcoming);
 
   return (
     <View style={styles.backgroundContainer}>
       <ScrollView style={styles.scrollView}>
         <View style={styles.container}>
-          <DataContext.Consumer>
-            {({ events }) => {
-              const now = new Date();
-              let sortedEvents = [...events];
+          {connectingToFirebase ? (
+            <Text>{NOTICES.CONNECT}</Text>
+          ) : (
+            <DataContext.Consumer>
+              {({ events }) => {
+                const now = new Date();
+                let sortedEvents = [...events];
 
-              if (filterType === EventsFilter.Upcoming) {
-                sortedEvents = sortedEvents.filter((event: any) => {
-                  return event.start && event.timezone
-                    ? moment(`${event.start}${event.timezone}`).toDate() > now
-                    : false;
-                });
+                if (filterType === EventsFilter.Upcoming) {
+                  sortedEvents = sortedEvents.filter((event: any) => {
+                    return event.start && event.timezone
+                      ? moment(`${event.start}${event.timezone}`).toDate() > now
+                      : false;
+                  });
 
-                sortedEvents.sort((a: any, b: any) => {
-                  if (a.start > b.start) {
-                    return 1;
-                  } else if (a.start < b.start) {
-                    return -1;
-                  }
-                  return 0;
-                });
-              } else if (filterType === EventsFilter.Past) {
-                sortedEvents = sortedEvents.filter((event: any) => {
-                  return event.start && event.timezone
-                    ? moment(`${event.start}${event.timezone}`).toDate() < now
-                    : false;
-                });
+                  sortedEvents.sort((a: any, b: any) => {
+                    if (a.start > b.start) {
+                      return 1;
+                    } else if (a.start < b.start) {
+                      return -1;
+                    }
+                    return 0;
+                  });
+                } else if (filterType === EventsFilter.Past) {
+                  sortedEvents = sortedEvents.filter((event: any) => {
+                    return event.start && event.timezone
+                      ? moment(`${event.start}${event.timezone}`).toDate() < now
+                      : false;
+                  });
 
-                sortedEvents.sort((a: any, b: any) => {
-                  if (a.start < b.start) {
-                    return 1;
-                  } else if (a.start > b.start) {
-                    return -1;
-                  }
-                  return 0;
-                });
-              }
+                  sortedEvents.sort((a: any, b: any) => {
+                    if (a.start < b.start) {
+                      return 1;
+                    } else if (a.start > b.start) {
+                      return -1;
+                    }
+                    return 0;
+                  });
+                }
 
-              if (sortedEvents.length > 0) {
-                return (
-                  <View>
-                    <View style={styles.filterContainer}>
-                      <Text>Фильтр</Text>
-                      <Button
-                        title="Предстоящие"
-                        onPress={() => setFilterType(EventsFilter.Upcoming)}
-                        style={
-                          filterType === EventsFilter.Upcoming
-                            ? styles.filterButtonFocused
-                            : styles.filterButton
-                        }
-                        selected={filterType === EventsFilter.Upcoming}
-                      />
-                      <Button
-                        title="Прошедшие"
-                        onPress={() => setFilterType(EventsFilter.Past)}
-                        style={
-                          filterType === EventsFilter.Past
-                            ? styles.filterButtonFocused
-                            : styles.filterButton
-                        }
-                        selected={filterType === EventsFilter.Past}
-                      />
-                    </View>
-                    {sortedEvents.map((event: any) => {
-                      return (
-                        <EventsListItem
-                          key={event.id}
-                          event={event}
-                          onPress={() => {
-                            navigation.navigate('EventSingleScreen', { event });
-                          }}
+                if (sortedEvents.length > 0) {
+                  return (
+                    <View>
+                      <View style={styles.filterContainer}>
+                        <Text>Фильтр</Text>
+                        <Button
+                          title="Предстоящие"
+                          onPress={() => setFilterType(EventsFilter.Upcoming)}
+                          style={
+                            filterType === EventsFilter.Upcoming
+                              ? styles.filterButtonFocused
+                              : styles.filterButton
+                          }
+                          selected={filterType === EventsFilter.Upcoming}
                         />
-                      );
-                    })}
-                  </View>
-                );
-              } else {
-                return (
-                  <View>
-                    <Text style={styles.emptyLabel}>Список пуст</Text>
-                  </View>
-                );
-              }
-            }}
-          </DataContext.Consumer>
+                        <Button
+                          title="Прошедшие"
+                          onPress={() => setFilterType(EventsFilter.Past)}
+                          style={
+                            filterType === EventsFilter.Past
+                              ? styles.filterButtonFocused
+                              : styles.filterButton
+                          }
+                          selected={filterType === EventsFilter.Past}
+                        />
+                      </View>
+                      {sortedEvents.map((event: any) => {
+                        return (
+                          <EventsListItem
+                            key={event.id}
+                            event={event}
+                            onPress={() => {
+                              navigation.navigate('EventSingleScreen', {
+                                event,
+                              });
+                            }}
+                          />
+                        );
+                      })}
+                    </View>
+                  );
+                } else {
+                  return (
+                    <View>
+                      <Text style={styles.emptyLabel}>Список пуст</Text>
+                    </View>
+                  );
+                }
+              }}
+            </DataContext.Consumer>
+          )}
         </View>
       </ScrollView>
     </View>
