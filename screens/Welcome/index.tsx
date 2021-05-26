f69584ac58b28f8b1348e6f80c1092903ee8b88b
Vadim Cpp <vadim.cpp@gmail.com>
2021-05-26 22:16:46 +0300

    #120 add buttons

--- screens/Welcome/index.tsx
+++ screens/Welcome/index.tsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
+import React, { useContext, useCallback } from 'react';
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 // components
 import NoDataContainer from '../../components/NoDataContainer';
 
 // constants
-import { NOTICE_CONNECTING, NOTICE_LOADING } from '../../utils/constants';
+import {
+  NOTICE_CONNECTING,
+  NOTICE_LOADING,
+  DEFAUTL_ACTIVE_OPACITY,
+} from '../../utils/constants';
 
 // contexts
 import AuthContext from '../../context/AuthContext';
@@ -21,27 +32,39 @@ export default function WelcomeScreen() {
   const { connectingToFirebase } = authContext;
   const { communities } = dataContext;
 
+  const onPress = useCallback((communityId: string) => {
+    console.log('TODO:', communityId);
+  }, []);
+
   return (
     <View style={styles.backgroundContainer}>
       <ScrollView style={styles.scrollView} bounces={false}>
-        <View style={styles.container}>
-          {connectingToFirebase || communities.length === 0 ? (
+        {connectingToFirebase || communities.length === 0 ? (
+          <View style={styles.container}>
             <NoDataContainer
               label={connectingToFirebase ? NOTICE_CONNECTING : NOTICE_LOADING}
             />
-          ) : (
-            <View>
-              <Text>Выберите сообщество:</Text>
-              {communities.map((community: ICommunity) => {
-                return (
-                  <View key={community.id}>
-                    <Text>{community.name}</Text>
-                  </View>
-                );
-              })}
-            </View>
-          )}
-        </View>
+          </View>
+        ) : (
+          <View style={styles.container}>
+            <Text>Выберите сообщество</Text>
+            {communities.map((community: ICommunity) => {
+              var base64Icon = `data:image/png;base64,${community.logo}`;
+              return (
+                <View key={community.id}>
+                  <TouchableOpacity
+                    onPress={() => onPress(community.id)}
+                    style={styles.button}
+                    activeOpacity={DEFAUTL_ACTIVE_OPACITY}
+                  >
+                    <Image style={styles.logo} source={{ uri: base64Icon }} />
+                    <Text style={styles.text}>{community.name}</Text>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
+          </View>
+        )}
       </ScrollView>
     </View>
   );
@@ -59,10 +82,34 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   container: {
+    width: '100%',
+    height: 500,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 30,
     marginTop: 30,
+  },
+  button: {
+    width: 310,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginTop: 30,
+  },
+  text: {
+    color: 'black',
+    fontSize: 32,
+  },
+  logo: {
+    width: 64,
+    height: 64,
+    borderColor: 'red',
+    marginLeft: 10,
+    marginRight: 10,
   },
 });
