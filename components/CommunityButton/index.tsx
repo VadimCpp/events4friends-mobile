3980f01c9d7e612c1c170661851ee9c35f0535ad
Вадим Канинский <vadim.cpp@gmail.com>
2021-07-20 19:46:35 +0300

    #123 add class decorator factory

--- components/CommunityButton/index.tsx
+++ components/CommunityButton/index.tsx
@@ -7,57 +7,85 @@ import { DEFAUTL_ACTIVE_OPACITY } from "../../utils/constants";
 // utils
 import { ICommunity } from '../../utils/interfaces';
 
-interface CommunityButtonProps {
-    community: ICommunity,
-    onPress: (id: string) => void,
+//
+// NOTE!
+// В целях изучения декораторов typescript был создан Decorator Factory,
+// который принимает в качестве параметра ширину границы и устанавливает ее.
+//
+// Лучшего примера применения классового декоратора придумать не смог.
+//
+// Пусть этот декоратор лежит пока в коде в ознакомительных целях.
+//
+// TODO: подумать над хорошим и уместным примером использования декоратора в коде
+//
+function BorderWidthDecorator(width: number) {
+  return <T extends { new (...args: any[]): {} }>(constructor: T) => {
+    return class extends constructor {
+      borderWidth = width;
+    };
+  }
 }
 
+interface CommunityButtonProps {
+  community: ICommunity,
+  onPress: (id: string) => void,
+}
+
+@BorderWidthDecorator(2)
 class CommunityButton extends React.Component<any, any> {
-    constructor(props: CommunityButtonProps) {
-        super(props);
-    }
+  constructor(props: CommunityButtonProps) {
+    super(props);
+    this.borderWidth = 1;
+  }
 
-    render() {
-        const { onPress, community: { logo, id, name } } = this.props;
+  readonly borderWidth: number;
 
-        const base64Icon = `data:image/png;base64,${logo}`;
+  render() {
+    const { onPress, community: { logo, id, name } } = this.props;
 
-        return (
-            <TouchableOpacity
-                onPress={() => onPress(id)}
-                style={styles.button}
-                activeOpacity={DEFAUTL_ACTIVE_OPACITY}
-            >
-                <Image style={styles.logo} source={{ uri: base64Icon }} />
-                <Text style={styles.text}>{name}</Text>
-            </TouchableOpacity>
-        );
-    }
+    const base64Icon = `data:image/png;base64,${logo}`;
+
+    return (
+      <TouchableOpacity
+        onPress={() => onPress(id)}
+        style={[
+          styles.button,
+          {
+            borderWidth: this.borderWidth,
+          }
+        ]}
+        activeOpacity={DEFAUTL_ACTIVE_OPACITY}
+      >
+        <Image style={styles.logo} source={{ uri: base64Icon }} />
+        <Text style={styles.text}>{name}</Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    button: {
-        width: 310,
-        height: 80,
-        borderRadius: 40,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        flexDirection: 'row',
-        borderColor: 'grey',
-        borderWidth: 1,
-        marginTop: 30,
-    },
-    text: {
-        color: 'black',
-        fontSize: 32,
-    },
-    logo: {
-        width: 64,
-        height: 64,
-        borderColor: 'red',
-        marginLeft: 10,
-        marginRight: 10,
-    },
+  button: {
+    width: 310,
+    height: 80,
+    borderRadius: 40,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginTop: 30,
+  },
+  text: {
+    color: 'black',
+    fontSize: 32,
+  },
+  logo: {
+    width: 64,
+    height: 64,
+    borderColor: 'red',
+    marginLeft: 10,
+    marginRight: 10,
+  },
 });
 
 export default CommunityButton;
