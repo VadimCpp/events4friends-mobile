804c9c55e5d883589d5f28079a96a9f402a233f7
Вадим Канинский <vadim.cpp@gmail.com>
2021-08-04 19:18:46 +0300

    #131 implement HeaderTitle

---
+++ components/HeaderTitle/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+interface HeaderTitleProps {
+  logo: string;
+  title: string;
+}
+
+const HeaderTitle = (props: HeaderTitleProps) => {
+  const { title, logo } = props;
+  const base64Icon: string = `data:image/png;base64,${logo}`;
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={{ uri: base64Icon }} />
+      <Text style={styles.title} numberOfLines={1}> {title} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 24,
+    height: 24,
+    borderColor: 'red',
+    marginRight: 5,
+  },
+  title: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default HeaderTitle;
