cb0e3116d8b50c23e56e6367cd6b732bd7611f25
Вадим Канинский <vadim.cpp@gmail.com>
2021-09-12 23:06:18 +0200

    #151 update babel config

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,5 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      development: {
-        plugins: [
-          [
-            'module-resolver',
-            {
-              root: ['./'],
-              alias: {
-                '@components': './components',
-                '@utils': './utils',
-                '@assets': './assets',
-                '@context': './context',
-              },
-            },
-          ],
-        ],
-      },
-    },
   };
 };
