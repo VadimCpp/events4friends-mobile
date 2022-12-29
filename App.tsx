0cee02875a3b17103bddde6826d451bafa6ccdd1
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-12-29 16:24:22 +0100

    #180 update old code

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View } from 'react-native';
 import { Asset } from 'expo-asset';
-import AppLoading from 'expo-app-loading';
+import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -23,6 +24,9 @@ import DataContext from './context/DataContext';
 import useAuth from './hooks/useAuth';
 import useData from './hooks/useData';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -35,28 +39,40 @@ export default function App() {
     loadingServices,
   } = useData();
 
-  const [isAppReady, setIsAppReady] = useState(false);
-  const [expoPushToken, setExpoPushToken] = useState('');
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const images = [
+          require('./assets/img/icon_place_x4.png'),
+          require('./assets/img/icon_telegram_x4.png'),
+          require('./assets/img/icon_time_x4.png'),
+          require('./assets/img/icon_viber_x4.png'),
+          require('./assets/img/icon_whatsapp_x4.png'),
+          require('./assets/img/icon_www_x4.png'),
+        ];
+    
+        const cacheImages = images.map(image => {
+          return Asset.fromModule(image).downloadAsync();
+        });
+        await (Promise.all(cacheImages) as unknown) as Promise<void>;
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   useEffect(() => {
     moment.locale('ru');
   }, []);
 
-  function cacheResourcesAsync(): Promise<void> {
-    const images = [
-      require('./assets/img/icon_place_x4.png'),
-      require('./assets/img/icon_telegram_x4.png'),
-      require('./assets/img/icon_time_x4.png'),
-      require('./assets/img/icon_viber_x4.png'),
-      require('./assets/img/icon_whatsapp_x4.png'),
-      require('./assets/img/icon_www_x4.png'),
-    ];
 
-    const cacheImages = images.map(image => {
-      return Asset.fromModule(image).downloadAsync();
-    });
-    return (Promise.all(cacheImages) as unknown) as Promise<void>;
-  }
   function storeReminder(
     value: boolean,
     eventId: string,
@@ -66,63 +82,77 @@ export default function App() {
     // Do nothing
   }
 
-  return isAppReady ? (
-    <AuthContext.Provider
-      value={{
-        user,
-        connectingToFirebase,
-      }}
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ minWidth: '100%', minHeight: '100%' }}
+      onLayout={onLayoutRootView}
     >
-      <DataContext.Provider
+      <AuthContext.Provider
         value={{
-          events,
-          services,
-          communities,
-          loadingEvents,
-          loadingServices,
-          storeReminder,
+          user,
+          connectingToFirebase,
         }}
       >
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                headerTitle: () => null,
-                headerLeft: () => null,
-                animationEnabled: false,
-              }}
-            />
-            <Stack.Screen
-              name="Details"
-              component={EventsScreen}
-              options={{ title: 'Все события' }}
-            />
-            <Stack.Screen
-              name="Services"
-              component={ServicesScreen}
-              options={{ title: 'Все услуги' }}
-            />
-            <Stack.Screen
-              name="EventScreen"
-              component={EventScreen}
-              options={{ title: 'Событие' }}
-            />
-            <Stack.Screen
-              name="ServiceScreen"
-              component={ServiceScreen}
-              options={{ title: 'Услуга' }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </DataContext.Provider>
-    </AuthContext.Provider>
-  ) : (
-    <AppLoading
-      startAsync={cacheResourcesAsync}
-      onFinish={() => setIsAppReady(true)}
-      onError={console.warn}
-    />
+        <DataContext.Provider
+          value={{
+            events,
+            services,
+            communities,
+            loadingEvents,
+            loadingServices,
+            storeReminder,
+          }}
+        >
+          <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  headerTitle: () => null,
+                  headerLeft: () => null,
+                  animationEnabled: false,
+                }}
+              />
+              <Stack.Screen
+                name="Details"
+                component={EventsScreen}
+                options={{ title: 'Все события' }}
+              />
+              <Stack.Screen
+                name="Services"
+                component={ServicesScreen}
+                options={{ title: 'Все услуги' }}
+              />
+              <Stack.Screen
+                name="EventScreen"
+                component={EventScreen}
+                options={{ title: 'Событие' }}
+              />
+              <Stack.Screen
+                name="ServiceScreen"
+                component={ServiceScreen}
+                options={{ title: 'Услуга' }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </DataContext.Provider>
+      </AuthContext.Provider>
+    </View>
   );
 }
